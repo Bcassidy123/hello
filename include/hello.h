088b6764fd1d4c3aebff49a2863b9a22cc97aca1
Barry Cassidy <barrycassidy123@tutanota.com>
2019-02-14 05:54:57 +1100

    initial

---
+++ include/hello.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+std::ostream &hello(std::ostream &os);
---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+std::ostream &hello(std::ostream &os) {
+  os << "Hello, World";
+  return os;
+}
