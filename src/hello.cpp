ec1c73046f5208bee1a0e04f65b10a4cc3019ada
Barry Cassidy <barrycassidy123@tutanota.com>
2019-02-15 04:16:27 +1100

    add string asset

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,10 +1,17 @@
 #include "hello.h"
 
 #include <fmt/format.h>
+#include <fstream>
 #include <string>
 
 std::ostream &hello(std::ostream &os) {
-  std::string s = fmt::format("{0}, World", "Goodbye");
+  std::ifstream f("assets/string.txt", std::ios::ate);
+  auto size = f.tellg();
+  f.seekg(0);
+  std::string buf(size, '\0');
+  f.read(buf.data(), size);
+
+  std::string s = fmt::format("{0}, World", buf);
   os << s;
   return os;
 }
